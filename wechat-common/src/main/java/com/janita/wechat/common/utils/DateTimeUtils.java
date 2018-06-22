d1b404df1e26681560594d8ca522263343adf31b
hspcadmin <880902>
2018-06-22 15:37:13 +0800

    获取到了openId

--- wechat-common/src/main/java/com/janita/wechat/common/utils/DateTimeUtils.java
+++ wechat-common/src/main/java/com/janita/wechat/common/utils/DateTimeUtils.java
@@ -1,4 +1,4 @@
-package utils;
+package com.janita.wechat.common.utils;
 
 
 import java.sql.Timestamp;
--- wechat-fuwuhao-server/src/main/java/com/janita/wechat/fuwuhao/task/FetchAccessTokenTask.java
+++ wechat-fuwuhao-server/src/main/java/com/janita/wechat/fuwuhao/task/FetchAccessTokenTask.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import utils.DateTimeUtils;
+import com.janita.wechat.common.utils.DateTimeUtils;
 
 import java.sql.Timestamp;
 
